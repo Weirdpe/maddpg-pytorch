65b4611e881df870ee34c0e7effded8e4b171aa8
Weirdpe <1264272596@qq.com>
2023-03-29 09:45:33 +0800

    maddpg

---
+++ utils/agents.py
@@ -0,0 +1,49 @@
+from torch import Tensor
+from torch.autograd import Variable
+from torch.optim import Adam
+from networks import MLPNetwork
+from noise import OUNoise
+
+class DDPGAgent(object):
+    def __init__(self, num_in_pol, num_out_pol, num_in_critic, hidden_dim=64,
+                 lr=0.01, discrete_action=True):
+        self.policy = MLPNetwork(num_in_pol, num_out_pol,
+                                 hidden_dim=hidden_dim,
+                                 constrain_out=True,
+                                 discrete_action=discrete_action)
+        self.critic = MLPNetwork(num_in_critic, 1,
+                                 hidden_dim=hidden_dim,
+                                 constrain_out=False)
+        self.target_policy = MLPNetwork(num_in_pol, num_out_pol,
+                                        hidden_dim=hidden_dim,
+                                        constrain_out=True,
+                                        discrete_action=discrete_action)
+        self.target_critic = MLPNetwork(num_in_critic, 1,
+                                        hidden_dim=hidden_dim,
+                                        constrain_out=False)
+
+        hard_update(self.target_policy, self.policy)
+        hard_update(self.target_critic, self.critic)
+        self.policy_optimizer = Adam(self.policy.parameters(), lr=lr)
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr)
+        if not discrete_action:
+            self.exploration = OUNoise(num_out_pol)
+        else:
+            self.exploration = 0.3
+        self.discrete_action = discrete_action
+
+    def reset_noise(self):
+        if not self.discrete_action:
+            self.exploration.reset()
+
+    def scale_noise(self, scale):
+        if self.discrete_action:
+            self.exploration = scale
+        else:
+            self.exploration.scale = scale
+
+    def step(self, obs, explore=False):
+        action = self.policy(obs)
+        if self.discrete_action:
+            if explore:
+                # action = gumbel_
